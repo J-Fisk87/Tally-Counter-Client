c8e2f468bb6fd4b0830cbc2d688b1ec9b78c899d
Tsura <kota@vaga.tk>
2020-05-27 14:27:44 -0500

    basic auth

---
+++ src/components/Home.js
@@ -0,0 +1,48 @@
+import React from "react";
+import axios from "axios";
+import { Link } from "react-router-dom";
+
+const Home = (props) => {
+  const handleClick = () => {
+    axios
+      .delete("http://localhost:3001/logout", { withCredentials: true })
+      .then((response) => {
+        props.handleLogout();
+        props.history.push("/");
+      })
+      .catch((error) => console.log(error));
+  };
+
+  return (
+    <div>
+      {props.loggedInStatus ? (
+        <div>
+          <Link to="/logout" onClick={handleClick}>
+            Log Out
+          </Link>
+        </div>
+      ) : (
+        <div>
+          <Link to="/login">Log In</Link>
+          <br></br>
+          <Link to="/signup">Sign Up</Link>
+          <br></br>
+        </div>
+      )}
+    </div>
+  );
+    
+};
+export default Home;
+
+{/* <div>
+      <Link to="/login">Log In</Link>
+      <br></br>
+      <Link to="/signup">Sign Up</Link>
+      <br></br>
+      {props.loggedInStatus ? (
+        <Link to="/logout" onClick={handleClick}>
+          Log Out
+        </Link>
+      ) : null}
+</div> */}
